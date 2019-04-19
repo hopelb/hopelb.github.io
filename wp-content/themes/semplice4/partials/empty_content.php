2bd1c0f9094e34597592fc5e766dbb72f29a1988
hopelb <37603779+hopelb@users.noreply.github.com>
2019-04-19 15:06:13 +1000

    Testing update Semplice

---
+++ wp-content/themes/semplice4/partials/empty_content.php
@@ -0,0 +1,4 @@
+
+<div class="no-content">
+	Empty content<br /><span>Before getting a preview of your page or project please add<br /> some content in our editor, save it and hit preview again.</span>
+</div>
---
+++ wp-content/themes/semplice4/partials/empty_semplice.php
@@ -0,0 +1,4 @@
+
+<div class="no-content">
+	Empty content<br /><span>It appears that you have a totally empty page. Please visit our editor and add some content to your page.</span>
+</div>
