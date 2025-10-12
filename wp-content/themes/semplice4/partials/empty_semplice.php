ec050db3f090bf9c909c64e380c6d95610fbf883
Bobby Ly <contact@hellobobbyly.com>
2025-10-12 16:38:33 +1100

    updated website, manual static remake

--- wp-content/themes/semplice4/partials/empty_semplice.php
+++
@@ -1,4 +0,0 @@
-
-<div class="no-content">
-	Empty content<br /><span>It appears that you have a totally empty page. Please visit our editor and add some content to your page.</span>
-</div>
--- wp-content/themes/semplice4/partials/no_posts.php
+++
@@ -1,4 +0,0 @@
-
-<div class="no-content">
-	No posts<br /><span>You have no posts in your blog. Start adding some posts in your dashboard.</span>
-</div>
